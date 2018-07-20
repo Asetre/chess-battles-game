7dbf01f7e500b01ecee948f6d298cd51ae6c16eb
Asetre <asetre.paul@gmail.com>
2018-07-20 01:38:06 -0700

    updates

---
+++ config.js
@@ -0,0 +1,4 @@
+const port = 8080
+const serverUrl = 'http://localhost:8000'
+
+module.exports = { port, serverUrl}
--- game.js
+++ game.js
@@ -1,15 +1,31 @@
-module.exports = function (io) {
-    io.on('connection', (socket) => {
-        //eslint-disable-next-line
-      console.log(`Socket: ${socket.id}`)
+const axios = require('axios')
+const {serverUrl} = require('./config')
 
-        socket.on('join room', (gameID) => {
-            socket.join(gameID)
-        })
-
-        socket.on('piece move', (data) => {
-            let { opponentSocketID } = data
-            socket.to(opponentSocketID).emit('opponent piece move', data)
-        })
+function main(io) {
+  io.on('connection', (socket) => {
+    socket.on('join room', (gameID) => {
+      socket.join(gameID)
     })
+
+    socket.on('piece move', (data) => {
+      io.to(data.gameID).emit('piece move', data)
+    })
+
+    socket.on('game over', (data) => {
+      if(!data) return
+      let url = `${serverUrl}/game/over`
+      console.log('sending')
+      axios.post(url, data)
+    })
+
+    socket.on('check opponent connection', (roomName) => {
+      let room = io.sockets.adapter.rooms[roomName]
+      if (io.sockets.adapter.sids[socket.id][roomName]) {
+        if (room.length < 2) socket.emit('opponent connection status', false)
+        else socket.emit('opponent connection status', true)
+      }
+    })
+  })
 }
+
+module.exports = main
